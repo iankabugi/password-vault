65f9941c66444277bfec454b43446c5421341aa8
Ian Kabugi <iank299@gmail.com>
2019-01-29 08:21:32 +0300

    add a username variable

--- Credential.py
+++ Credential.py
@@ -13,12 +13,13 @@ class Credential:
 
         credential.credential_list.append(self)
 
-    def __init__(self,first_name,last_name,number,email):
+    def __init__(self,first_name,last_name,username,number,email):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
+        self.username = username
         self.phone_number = number
         self.email = email
 #code to delete credential      
